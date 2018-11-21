64c10f0a7dbf7e6f54a0d50b07f64af114f76713
ismailalkouz94 <ismailkouz2015@gmail.com>
2018-11-21 12:33:38 +0200

    add session factory bean

--- src/main/java/com/lg/JepcoCsPortal/JepcoCsPortalApplication.java
+++ src/main/java/com/lg/JepcoCsPortal/JepcoCsPortalApplication.java
@@ -2,11 +2,18 @@ package com.lg.JepcoCsPortal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
 
 @SpringBootApplication
 public class JepcoCsPortalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JepcoCsPortalApplication.class, args);
+	}
+
+	@Bean
+	public HibernateJpaSessionFactoryBean sessionFactory(){
+		return new HibernateJpaSessionFactoryBean();
 	}
 }
