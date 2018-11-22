9f690da249d7af947b30b4b27f2a0902a4f3e922
ismailalkouz94 <ismailkouz2015@gmail.com>
2018-11-22 09:58:32 +0200

    add external tomcat config

--- src/main/java/com/lg/JepcoCsPortal/JepcoCsPortalApplication.java
+++ src/main/java/com/lg/JepcoCsPortal/JepcoCsPortalApplication.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
@@ -17,10 +19,16 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import javax.persistence.EntityManagerFactory;
 
 @SpringBootApplication
-public class JepcoCsPortalApplication {
+public class JepcoCsPortalApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JepcoCsPortalApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(JepcoCsPortalApplication.class);
+	}
+
 
 }
